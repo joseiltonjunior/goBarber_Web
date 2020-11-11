4f670d4ccc1ac88d4904876ac37db9163b03efc8
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-11-11 16:48:56 -0300

    implementation page sing in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logo from '../../assets/Logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 import { Container, Content, Background } from './styles';
 
@@ -10,9 +12,10 @@ const SignIn: React.FC = () => (
     <Content>
       <img src={logo} alt="goBarber" />
       <form>
-        <input placeholder="E-mail" />
-        <input placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input name="password" icon={FiLock} placeholder="Senha" />
+
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export default createGlobalStyle`
 }
 
 body {
-  background: #312E38;
+  background:#262228;
   color: #FFF;
   -webkit-font-smoothing: antialiased;
 }
